e7cbdb035ec860c41ecac7172b374e33c7614c10
J-SUPHA <84354729+J-SUPHA@users.noreply.github.com>
2021-07-13 11:59:14 -0700

    Update Timer.java

--- MMA timer/src/Timer.java
+++ MMA timer/src/Timer.java
@@ -10,9 +10,9 @@ public class Timer {
 	
 	public static void main(String [] args) throws InterruptedException, UnsupportedAudioFileException, IOException {
 
-		Input b = new Input();
-		int [] data = new int [5];
-		b.store = data;
+		Input b = new Input(); //instance of java.swing class
+		int [] data = new int [5];//where users inputs go
+		b.store = data; // getting the inputs from the user 
 		
 		
 	
@@ -21,27 +21,27 @@ public class Timer {
 		String Path3 = "E.wav";
 		
 		
-		timer(data[0], data[1], data[2], data[3], data[4], Path1, Path2, Path3);
+		timer(data[0], data[1], data[2], data[3], data[4], Path1, Path2, Path3); //passing through the timer
 		
 	}
 	public static void timer(int min1, int sec1, int min2, int sec2, int rounds, String A, String B,String C) throws InterruptedException, UnsupportedAudioFileException, IOException {
 		
-		int tot1 = (min1*60)+sec1;
+		int tot1 = (min1*60)+sec1; //conversion to seconds 
 		int T1 = tot1;
-		int tot2 = (min2*60)+sec2;
+		int tot2 = (min2*60)+sec2; //conversion seconds
 		int T2 = tot2;
 		
-		int counter1 = duration(A);
+		int counter1 = duration(A); //to get the duration of time needed to play the sound recordings (coded so that sound recording don't count in terms of time)
 		int counter2 = duration(B);
 		int counter3 = duration(C);
 		
 		while(rounds > 0) {
-			T1 = tot1;
+			T1 = tot1; // reset timer after each round is complete
 			T2 = tot2;
-			while (T1>0) {
+			while (T1>0) { //while loop that goes through the timer
 				if (T1==tot1 && rounds == 1) {
 					playmusic(C,counter3);
-					System.out.println("Final Round Start");
+					System.out.println("Final Round Start");//trackers 
 				}
 				else if (T1 == tot1) {
 					playmusic(A,counter1);
@@ -63,12 +63,12 @@ public class Timer {
 		
 	}
 	public static void playmusic(String s, int t) {
-		Music ob = new Music();
+		Music ob = new Music(); //start playing music
 		ob.playmusic(s,t);
 		System.out.println("Playing");
 	}
 	public static int duration(String s) throws UnsupportedAudioFileException, IOException {
-		int t;
+		int t; // for conversion from float to string
 		File file = new File(s);
 		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
 		AudioFormat format = audioInputStream.getFormat();
